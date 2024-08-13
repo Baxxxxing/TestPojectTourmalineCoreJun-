fb12d08fb202e1dc60eba4847a88f054f9bc3262
Baxxxxing <renev-maksimka95@mail.ru>
2024-08-13 20:06:10 +0500

    Добавьте файлы проекта.

---
+++ TestPojectTourmlineCoreJune/ChangeCalculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestPojectTourmlineCoreJune
+{
+    public class ChangeCalculator
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
