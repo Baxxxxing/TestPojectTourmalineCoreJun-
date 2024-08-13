0fd466e7b2aa88c464b0bf32845fbc77eab49862
Baxxxxing <renev-maksimka95@mail.ru>
2024-08-13 20:10:56 +0500

    A solution has been created containing the project "TestPojectTourmlineCoreJune" responsible for the implementation of the functional part, as well as the project "TestPojectTourmlineCoreJuneTests" responsible for testing the functional part.

---
+++ TestPojectTourmlineCoreJuneTests/ChangeCalculatorTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestPojectTourmlineCoreJune;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestPojectTourmlineCoreJune.Tests
+{
+    [TestClass()]
+    public class ChangeCalculatorTests
+    {
+        [TestMethod()]
+        public void GetChangeTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ TestPojectTourmlineCoreJuneTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью указанного ниже 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// общие сведения об этой сборке.
+[assembly: AssemblyTitle("TestPojectTourmlineCoreJuneTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestPojectTourmlineCoreJuneTests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка параметра ComVisible в значение false делает типы в этой сборке невидимыми 
+// для компонентов модели COM.  Если тип в этой сборке необходимо сделать доступным 
+// модели COM, задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("9ff9882a-39c6-4d6e-8c94-d41fbee6d4b8")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [сборка: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
